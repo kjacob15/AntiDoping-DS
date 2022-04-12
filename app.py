0f2a4b86474588c36c15d591a453037c1ceaf3be
kjacob15 <91503607+kjacob15@users.noreply.github.com>
2022-04-12 17:19:54 +0100

    Changes to make it run from app.py

--- app.py
+++ app.py
@@ -4,19 +4,8 @@ from flask_cors import CORS
 from flask import Flask, render_template, request, jsonify, redirect, url_for, send_from_directory
 from Models.athlete import Athlete
 from extensions import db
-from api.Landingpage import landingPage
-from api.Athlete import athlete
-
-
-
-
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
-
-app.register_blueprint(landingPage)
-app.register_blueprint(athlete)
-
-
+# from api.Landingpage import landingPage
+# from api.Athlete import athlete
 
 
 
@@ -24,68 +13,79 @@ app.register_blueprint(athlete)
 # app = Flask(__name__)
 # CORS(app, resources={r"/*": {"origins": "*"}})
 
+# app.register_blueprint(landingPage)
+# app.register_blueprint(athlete)
 
-# @app.route('/')
-# def index():
-#    print('Request for index page received')
-#    return render_template('index.html')
 
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-# @app.route('/hello', methods=['POST'])
-# def hello():
-#    name = request.form.get('name')
 
-#    if name:
-#        print('Request for hello page received with name=%s' % name)
-#        return render_template('hello.html', name = name)
-#    else:
-#        print('Request for hello page received with no name or blank name -- redirecting')
-#        return redirect(url_for('index'))
 
-# @app.route('/register_athlete', methods=['POST'])
-# def register_athlete():
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+@app.route('/register_athlete', methods=['POST'])
+def register_athlete():
     
-#     response = {}
+    response = {}
 
-#     try:
-#         today = date.today()
-#         # timestamp = date.now()
+    try:
+        today = date.today()
+        # timestamp = date.now()
 
-#         email_ = request.form['email']
-#         name_ = request.form['name']
-#         location_ = request.form['location']
-#         time_= request.form['time']
-#         date_ = today.strftime("%d/%m/%Y")
-
-
-#         athlete_collection = db['athlete']
+        email_ = request.form['email']
+        name_ = request.form['name']
+        location_ = request.form['location']
+        time_= request.form['time']
+        date_ = today.strftime("%d/%m/%Y")
 
 
-#         query = {"email":email_}
-#         values= athlete_collection.find({ "email": { "$eq": email_} })
-#         if(len(list(values))>0):
-#             response['status'] = 400
-#             response['message'] = 'Athlete entry already exists'
-#             return jsonify(response)
+        athlete_collection = db['athlete']
 
 
-#         new_athlete = Athlete(email=email_, name=name_,
-#                               location=location_, date=date_, time=time_).make_dict()
-#         athlete_collection.insert_one(new_athlete)
+        query = {"email":email_}
+        values= athlete_collection.find({ "email": { "$eq": email_} })
+        if(len(list(values))>0):
+            response['status'] = 400
+            response['message'] = 'Athlete entry already exists'
+            return jsonify(response)
 
-#         response['status'] = 200
-#         response['message'] = 'Athlete registered successfully'
-#         return jsonify(response)
 
-#     except Exception as e:
-#         print(e)
-#         response['status'] = 400
-#         response['message'] = 'Athlete could not be registered'
-#         return jsonify(response)
+        new_athlete = Athlete(email=email_, name=name_,
+                              location=location_, date=date_, time=time_).make_dict()
+        athlete_collection.insert_one(new_athlete)
+
+        response['status'] = 200
+        response['message'] = 'Athlete registered successfully'
+        return jsonify(response)
+
+    except Exception as e:
+        print(e)
+        response['status'] = 400
+        response['message'] = 'Athlete could not be registered'
+        return jsonify(response)
 
 
 
